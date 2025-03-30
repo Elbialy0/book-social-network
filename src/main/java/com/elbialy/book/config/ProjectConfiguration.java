f84489ec5881f0a946e79acdc7750ee7cfd0c2a9
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-03-31 00:58:04 +0200

    Add the application auditor aware

--- src/main/java/com/elbialy/book/config/ProjectConfiguration.java
+++ src/main/java/com/elbialy/book/config/ProjectConfiguration.java
@@ -3,6 +3,7 @@ package com.elbialy.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -34,5 +35,9 @@ public class ProjectConfiguration {
         return new ProviderManager(authenticationProvider);
     }
 
+    @Bean
+    public AuditorAware<Integer> auditorAware(){
+        return new ApplicationAuditAware();
+    }
 
 }
